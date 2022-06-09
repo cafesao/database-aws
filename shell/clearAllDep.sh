3019a3fbb77069dac6f8f06ce2b85bd9632931eb
cafesao <my@cafesao.net>
2022-06-09 11:40:07 -0300

    Fix: Clear All Dep

--- shell/clearAllDep.sh
+++ shell/clearAllDep.sh
@@ -5,13 +5,13 @@ echo $ROOT_FOLDER
 
 # Install Modules (Typescript)
 
-cd Lambdas/Create && rm -r ./node_modules ./dist && echo "Clear Create"
+cd Lambdas/Create && rm -r ./node_modules ./dist *.zip* ./nodejs && echo "Clear Create"
 cd $ROOT_FOLDER
-cd Lambdas/Read && rm -r ./node_modules ./dist && echo "Clear Read"
+cd Lambdas/Read && rm -r ./node_modules ./dist *.zip* ./nodejs && echo "Clear Read"
 cd $ROOT_FOLDER
-cd Lambdas/Update && rm -r ./node_modules ./dist && echo "Clear Update"
+cd Lambdas/Update && rm -r ./node_modules ./dist *.zip* ./nodejs && echo "Clear Update"
 cd $ROOT_FOLDER
-cd Lambdas/Delete && rm -r ./node_modules ./dist && echo "Clear Delete"
+cd Lambdas/Delete && rm -r ./node_modules ./dist *.zip* ./nodejs && echo "Clear Delete"
 cd $ROOT_FOLDER
-cd Lambdas/Layer && rm -r ./node_modules ./dist ./nodejs && echo "Clear Layer"
+cd Lambdas/Layer && rm -r ./node_modules ./dist *.zip* ./nodejs && echo "Clear Layer"
 cd $ROOT_FOLDER
